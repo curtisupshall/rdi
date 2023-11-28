8523873589fe34fb0525fc8d2825e34e59f24065
Curtis Upshall <curtisupshall@gmail.com>
2023-11-27 17:21:44 -0800

    Fix int32 type

--- src/repeat.codon.py
+++ src/repeat.codon.py
@@ -1,28 +1,16 @@
 from bio import *
 
 LIBDIVSUFSORT="lib/libdivsufsort.so.3"
-from C import LIBDIVSUFSORT.divsufsort(ptr[byte], ptr[int], int)
-
-
-
-class RDIndex:
-
-    def query(l: int, r: int):
-        return None
-    
+from C import LIBDIVSUFSORT.divsufsort(ptr[byte], Ptr[Int[32]], int)
 
 def main():
     Text = "abracadabra"
     n = len(Text)
-    SA = Ptr[int](n)
-
-    print(f'n = {n}')
-
+    SA = Ptr[Int[32]](n)
     divsufsort(Text.c_str(), SA, n)
 
     for i in range(n):
         print(f'SA[{i}] = {SA[i]}')
-
 
 
 main()
