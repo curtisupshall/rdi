8814c7b95057b5f35867d531fe593202b11de545
Curtis Upshall <curtisupshall@gmail.com>
2023-12-03 20:39:32 -0800

    Fix LCP array construction; code cleanup

--- src/repeat.codon.py
+++ src/repeat.codon.py
@@ -5,18 +5,12 @@ from C import LIBDIVSUFSORT.divsufsort(ptr[byte], Ptr[Int[32]], int)
 
 # Kasai Algorithm
 def build_lcp_array(s: str, n, suffix_array: Ptr[Int[32]]) -> List[int]:
+    lcp = List[int]([0] * n)
     rank = List[int]([0] * n)
+    k = 0
 
     for i in range(n):
         rank[int(suffix_array[i])] = i
-
-    k = 0
-    lcp = List[int]()
-
-    print(f'Init lcp: {lcp}')
-
-    # for i in range(n):
-    #     lcp[i] = 0
 
     for i in range(n):
         if rank[i] == n - 1:
@@ -36,8 +30,8 @@ def main():
     Text = "abracadabra"
     n = len(Text)
     SA = Ptr[Int[32]](n)
+    
     divsufsort(Text.c_str(), SA, n)
-
     lcp_array = build_lcp_array(Text, n, SA)
 
     print('---- Suffix Array ----')
@@ -45,14 +39,6 @@ def main():
         print(f'SA[{i}] = {SA[i]}')
 
     print('LCP array:')
-    # print(lcp_array)
-
-    # print('Rank')
-    # rank = List[Int[32]]([Int[32](0)] * n)
-    # print(rank)
-
-    # test = Array[Int[32]](12)
-
-    # print(test)
+    print(lcp_array)
 
 main()
