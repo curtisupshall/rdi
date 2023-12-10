bad186a952fa8fd5ecd6a22e6b90a7cc637bb384
Curtis Upshall <curtisupshall@gmail.com>
2023-12-10 10:46:13 -0800

    Working BTree

--- src/btree.codon.py
+++ src/btree.codon.py
@@ -1,3 +1,5 @@
+from python import json
+
 class TreeNode[T]:
     key: int
     value: T
@@ -25,12 +27,58 @@ class TreeNode[T]:
             else:
                 self.right.child(key, value)
     
-    def serialize(self):
-        with open(filename, 'w') as file:
-            json.dump(serialized_tree, file)
+    def find(self, key):
+        if self.key == key:
+            return self.value
+        elif key < self.key and self.left is not None:
+            return self.left.find(key)
+        elif self.right is not None:
+            return self.right.find(key)
+        
+        return None
+
+    def serialize(self) -> str:
+        left = self.left.serialize() if self.left else 'null'
+        right = self.right.serialize() if self.right else 'null'
+
+        return f'[{self.key}, {json.dumps(self.value)}, {left}, {right}]'
+
+    @staticmethod
+    def deserialize(body):
+        return
+    
+class BTree[T]:
+    root: Optional[TreeNode[T]] = None
+
+    def __init__(self):
+        self.root = None
+
+    def __repr__(self):
+        return None if self.root is None else self.root.serialize()
+    
+    def insert(self, key: int, value: T):
+        if self.root is None:
+            self.root = TreeNode(key, value)
+        else:
+            self.root.child(key, value)
+
+    def search(self, key):
+        return None if self.root is None else self.root.find(key)
 
     
-node = TreeNode[str](10, 'hello')
-node.child(2, 'world')
+# node = TreeNode[str](10, 'hello')
+# node.child(2, 'world')
 
-print(node)
+# print(node.serialize())
+
+tree = BTree[List[int]]()
+
+tree.insert(5, [5, 6])
+tree.insert(3, [3, 4])
+tree.insert(2, [2, 3])
+tree.insert(9, [9, 10])
+tree.insert(8, [8, 9])
+
+print(tree)
+
+print(tree.search(2))
